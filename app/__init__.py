49dcc96acf7ceb9ae68058be322d974476e79851
Kimotho <plkimotho@gmail.com>
2019-01-09 14:06:09 +0300

    [finishes #163081193] Registers blueprint to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import Flask, Blueprint
+
+from app.api.v1 import version_one as v1
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.register_blueprint(v1)
     return app
