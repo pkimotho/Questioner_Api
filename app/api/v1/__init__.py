0bcb8f4c7bdee563ff68e2711c0a57bc164e2a8f
Kimotho <plkimotho@gmail.com>
2019-01-11 07:20:17 +0300

    [finishes #163045501] Creates questions view blueprint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,9 +3,12 @@ from flask_restful import Resource, Api
 
 # Local imports
 from app.api.v1.views.meetup_view import MeetUps
+from app.api.v1.views.question_view import Questions
 
 version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 api = Api(version_one)
 
 api.add_resource(MeetUps, '/meetups')
+
+api.add_resource(Questions, '/questions')
