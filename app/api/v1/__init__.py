e85352dc350492cc93f645b886a6a1732b650f4e
Kimotho <plkimotho@gmail.com>
2019-01-14 12:31:35 +0300

    [feature #163045428] Adds path for individual meetup

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,10 +2,11 @@ from flask import Blueprint
 from flask_restful import Resource, Api
 
 # Local imports
-from app.api.v1.views.meetup_view import MeetUps
+from app.api.v1.views.meetup_view import MeetUps, MeetUpItem
 
 version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 api = Api(version_one)
 
 api.add_resource(MeetUps, '/meetups')
+api.add_resource(MeetUpItem, '/meetups/<int:id>')
